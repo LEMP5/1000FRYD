163b114b748164931b7c5ee9816c4a8ace2f94ec
Pavel Andrei <pavelhandup@gmail.com>
2015-05-21 18:39:12 +0200

    Changes still being made.

--- 1000FRYD/src/ControlLayer/CtrDay.java
+++ 1000FRYD/src/ControlLayer/CtrDay.java
@@ -1,32 +1,33 @@
 package ControlLayer;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 
 import ModelLayer.Day;
+import DBLayer.DbDay;
 
 public class CtrDay {
+	DbDay dbDay;
 	private ArrayList<Day> week;
 	private String[] daysOfWeek;
-	private Day today;
 
 	public CtrDay() {
 		super();
+		dbDay = new DbDay();
 		week = new ArrayList<Day>();
 		daysOfWeek = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 	}
 	
-	public void createDay(int weeksAhead){
+	public void createDays(int weeksAhead){
 		Calendar calendar = Calendar.getInstance();
         int t = calendar.get(Calendar.DAY_OF_WEEK)-1;
         calendar.add(Calendar.DAY_OF_MONTH, -(t) + 7 * (weeksAhead));
+        Day dayObj;
         for(int i = 0; i <7; i++){
         	String date = "" + calendar.get(Calendar.YEAR) + "-" + calendar.get(Calendar.MONTH) + "-" + calendar.get(Calendar.DAY_OF_MONTH);
-        	new Day(date, daysOfWeek[i]);
+        	dayObj = new Day(date, daysOfWeek[i]);
+        	week.add(dayObj);
+        	dbDay.getDay(date, daysOfWeek[i]);
         	calendar.add(Calendar.DAY_OF_MONTH, 1);
         }
 	}
--- 1000FRYD/src/DBLayer/DbDay.java
+++ 1000FRYD/src/DBLayer/DbDay.java
@@ -14,22 +14,29 @@ public class DbDay {
     public DbDay() {
       con = DbConnection.getInstance().getDBcon();
     }
-    
-    public ArrayList<Day> getAllDays(boolean retriveAssociation)
-    {
-        return miscWhere("", retriveAssociation);
+    public Day getDay(String date, String dayOfWeek){
+    	Day dayObj = findDay(date, false);
+    	if(dayObj== null)
+			try {
+				insertDay(date, dayOfWeek);
+				return findDay(date, false);
+			} catch (Exception e) {
+				System.out.println("Error while ");
+			}
+    	return dayObj;
+    	
     }
     public Day findDay(String date, boolean retriveAssociation)
     {   String wClause = "  date = '" + date + "'";
         return singleWhere(wClause, retriveAssociation);
     }
-    public int insertDay(Day day) throws Exception
+    public int insertDay(String date, String dayOfWeek) throws Exception
     {   
   
        int rc = -1;
 	   String query="INSERT INTO day(date, dayOfWeek)  VALUES('"+
-	  		day.getDate()  + "','"  +
-		    day.getDayOfWeek()  + "')";
+	  		date  + "','"  +
+		    dayOfWeek  + "')";
       try{
           Statement stmt = con.createStatement();
           stmt.setQueryTimeout(5);
@@ -64,7 +71,7 @@ public class DbDay {
 	private Day singleWhere(String wClause, boolean retrieveAssociation)
 	{
 		ResultSet results;
-		Day day = new Day();
+		Day dayObj = new Day();
                 
 	        String query =  buildQuery(wClause);
 		try{ 
@@ -73,65 +80,42 @@ public class DbDay {
 	 		results = stmt.executeQuery(query);
 	 		
 	 		if( results.next() ){
-                            empObj = buildEmployee(results);
+                            dayObj = buildDay(results);
                             //assocaition is to be build
                             stmt.close();
                             if(retrieveAssociation)
-                            {   //The supervisor and department is to be build as well
-                                String superssn = empObj.getSupervisor().getSsn();
-                                Employee superEmp = singleWhere(" ssn = '" + superssn + "'",false);
-                                empObj.setSupervisor(superEmp);
-                                System.out.println("Supervisor is seleceted");
-                               // here the department has to be selected as well
-                           
-                           
+                            {
                             }
 			}
                         else{ //no employee was found
-                            empObj = null;
+                            dayObj = null;
                         }
 		}//end try	
 	 	catch(Exception e){
 	 		System.out.println("Query exception: "+e);
 	 	}
-		return empObj;
+		return dayObj;
 	}
-	//method to build the query
 	private String buildQuery(String wClause)
 	{
-	    String query="SELECT fname, minit,lname,ssn, address, bdate,sex, salary, superssn,dno  FROM employee";
+	    String query="SELECT date, dayOfWeek FROM day";
 		
 		if (wClause.length()>0)
 			query=query+" WHERE "+ wClause;
 			
 		return query;
 	}
-	//method to build an employee object
-	private Employee buildEmployee(ResultSet results)
-      {   Employee empObj = new Employee();
-          try{ // the columns from the table emplayee  are used
-                empObj.setFname(results.getString("fname"));
-                empObj.setMinit(results.getString("minit"));
-                empObj.setLname(results.getString("lname"));
-                empObj.setSsn(results.getString("ssn"));
-                empObj.setBdate(results.getString("bdate"));
-                empObj.setAddress(results.getString("address"));
-                empObj.setSex(results.getString("sex"));
-                empObj.setSalary(results.getDouble("salary"));
-                empObj.setSupervisor(new Employee(results.getString("superssn")));
-                empObj.setDepartment(new Department(results.getInt("dno")));
+	private Day buildDay(ResultSet results)
+      {   Day dayObj = new Day();
+          try{
+                dayObj.setDate(results.getString("date"));
+                dayObj.setDayOfWeek(results.getString("dayOfWeek"));
           }
          catch(Exception e)
          {
-             System.out.println("error in building the employee object");
+             System.out.println("error in building the day object");
          }
-         return empObj;
+         return dayObj;
       }
-
-	@Override
-	public Employee searchEmployeeSsn(String ssn, boolean b) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
